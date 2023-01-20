3b41af066255eb9943d03ca572c4ab4a51962ae6
Lucas Felix <lucariozin.net@gmail.com>
2023-01-20 14:51:08 -0300

    creating the 'create new task' feature.

--- src/components/Header/components/AddNewTaskForm/AddNewTaskForm.tsx
+++ src/components/Header/components/AddNewTaskForm/AddNewTaskForm.tsx
@@ -1,10 +1,30 @@
+import { FormEvent, useState } from 'react'
 import { PlusCircle } from 'phosphor-react'
+
+import { useTasks } from '@contexts/Tasks'
+
 import { Button, ButtonText, Container, Input } from './AddNewTaskForm.styles'
 
 export const AddNewTaskForm = () => {
+  const [taskName, setTaskName] = useState('')
+  const { createNewTask } = useTasks()
+
+  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    if (!taskName.length) return
+
+    createNewTask({ taskName })
+  }
+
   return (
-    <Container>
-      <Input type="text" placeholder="Adicione uma nova tarefa" />
+    <Container onSubmit={handleFormSubmit}>
+      <Input
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+        value={taskName}
+        onChange={(e) => setTaskName(e.target.value)}
+      />
 
       <Button type="submit">
         <ButtonText>Criar</ButtonText>
